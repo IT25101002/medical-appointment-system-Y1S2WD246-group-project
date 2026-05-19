0841526d095205a76734dafedc60381eb34203ac
IT25101567 <it25101567@my.sliit.lk>
2026-05-19 20:46:08 +0530

    Delete src/main/java/com/medical/medischeduler/patient/PatientRepository.java

--- src/main/java/com/medical/medischeduler/patient/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medical.medischeduler.patient;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-    Patient findByEmail(String email);
-}
