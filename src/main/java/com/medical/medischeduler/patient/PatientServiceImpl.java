3ded4c3dace64618244e5b0c4397800b7537efe8
IT25101567 <it25101567@my.sliit.lk>
2026-05-19 21:51:51 +0530

    Add Patient CRUD module (backend and Thymeleaf templates)

---
+++ src/main/java/com/medical/medischeduler/patient/PatientServiceImpl.java
@@ -0,0 +1,38 @@
+package com.medical.medischeduler.patient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Override
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    @Override
+    public Patient getPatientById(Long id) {
+        return patientRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    @Override
+    public void deletePatient(Long id) {
+        patientRepository.deleteById(id);
+    }
+
+    @Override
+    public Patient findByEmail(String email) {
+        return patientRepository.findByEmail(email);
+    }
+}
